86ccbbda91742e84a97cf7cce50b01999a7d465c
Olli Jokinen <olli.jokinen@snowfox.ai>
2023-04-14 07:58:31 +0200

    [WE-2] notebooks, scripts

---
+++ scripts/build_citations.py
@@ -0,0 +1,68 @@
+import glob
+import json 
+from tqdm import tqdm
+
+
+def get_files():
+    wiki_files = glob.glob("./text/*/wiki*")
+    return wiki_files 
+
+def get_metadata():
+    with open("metadata.json", "r") as f:
+        data = json.load(f)
+    return data 
+
+output = {}
+
+
+def main():
+
+    '''
+    Some of the articles are empty.
+    metadata.json should contain only non empty articles
+    => if cited article exists an has text => keep it, other wise dump it.
+    '''
+    valid_articles = get_metadata()
+    skip_cache = {}
+    cited_articles_not_in_metadata = []
+    wiki_files = get_files()
+    for wiki in tqdm(wiki_files):
+        with open(wiki, "r") as f:
+            data = f.read()
+
+        for sample in data.splitlines():
+            sample = json.loads(sample)
+            sample_id = sample["id"]
+            if valid_articles.get(sample_id) is None:
+                continue
+            reference_list = sample["introduction_references"]
+            if len(reference_list) > 0:
+                reference_list = reference_list[0]
+                direct_citations = []
+                for item in reference_list:
+                    article_id = item["article_id"]
+                    if article_id == "##NOT_FOUND##":
+                        continue
+                    """
+                    Do not add articles that does not exist in metadata.json 
+                    """
+                    if valid_articles.get(article_id) is None:
+                        cited_articles_not_in_metadata.append(article_id)
+                        continue
+
+                    direct_citations.append(article_id)
+
+                output[sample_id] = {article_id: {"count":5} for article_id in direct_citations}
+            else:
+                output[sample_id] = {}
+
+    output_path = "direct_citations.json"
+    with open(output_path, "w") as f:
+        f.write(json.dumps(output))
+
+    with open("directly_cited_articles_not_in_metadata.json", "w") as f:
+        f.write(json.dumps(cited_articles_not_in_metadata))
+
+
+if __name__ == "__main__":
+    main()
---
+++ scripts/build_metadata.py
@@ -0,0 +1,73 @@
+import glob 
+import json 
+def get_files():
+  print("Reading all wiki files...")
+  wiki_files = glob.glob("./text/*/wiki*")
+  print("Done reading.")
+  return wiki_files 
+
+valid_ids = set()
+
+
+def build_metadata():
+  global valid_ids
+  wiki_files = get_files()
+  metadata = {}
+  for i,wiki in enumerate(wiki_files):
+    print(f"Handling {i+1}/{len(wiki_files)}, {wiki}")
+    with open(wiki, "r") as f:
+      data = f.read()
+    metadata = handle_wiki(data, metadata)
+  output_path = "metadata.json"
+  write_data(output_path, metadata)
+  write_data("valid_ids.json", list(valid_ids))
+
+
+def write_data(output_path, data):
+  print("Saving data to path {}".format(output_path))
+  with open(output_path, "w") as f:
+    f.write(json.dumps(data))
+  print("Saved.")
+
+
+def handle_wiki(data, metadata):  
+  global valid_ids
+  for sample in data.splitlines():
+    try:
+      sample = json.loads(sample)
+    except Exception as e:
+      raise Exception(e)
+    paper_id = sample["id"]
+
+    try:
+      introduction_list_of_sentences = sample["introduction"][0] #list of list
+    except IndexError:
+      introduction_list_of_sentences = sample["introduction"]
+    #print(introduction_list_of_sentences); break;
+    introduction_text = " ".join(introduction_list_of_sentences) #join sentences
+    title = sample["title"]
+
+    character_count_filter = 50
+    if len(introduction_text) < character_count_filter: #skip introductions that are less than 50 characters. Do configurable.
+      print("Skipping article")
+      pass
+    elif "(täsmennyssivu)" in title:
+      pass
+    else:
+      metadata[paper_id] = {}
+      metadata[paper_id]["abstract"] = introduction_text
+      metadata[paper_id]["title"] = title
+      metadata[paper_id]["paper_id"] = paper_id
+      valid_ids.add(paper_id)
+  return metadata
+
+build_metadata()
+
+def main():
+  build_metadata()
+
+
+if __name__ == "__main__":
+  main()
+   
+
